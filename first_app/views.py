79d0da078bf3a29ca5723602e1fc77d087e2ecc1
ChristinaGaitan <christina.gaitan@gmail.com>
2021-01-11 20:21:34 -0600

    Create index template and modify index view to return a dictionary

--- first_app/views.py
+++ first_app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-  return HttpResponse('<strong>Hello World!</strong>')
+  date_dictionary = {'insert_me': 'Hello I am from view.py!'}
+  return render(request, 'first_app/index.html', context=date_dictionary)
